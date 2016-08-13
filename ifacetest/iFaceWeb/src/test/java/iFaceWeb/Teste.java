e59254bf1734908f3efe38556add9bd45cbef73d
Vanessa <vsv@ic.ufal.br>
2016-08-13 13:04:01 -0300

    refactoring

---
+++ ifacetest/iFaceWeb/src/test/java/iFaceWeb/Teste.java
@@ -0,0 +1,10 @@
+package iFaceWeb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("index")
+public class Teste {
+
+}
